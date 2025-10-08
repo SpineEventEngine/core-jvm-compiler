4b54d7c4464e3df0e695e31abab3fbba73dbba36
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-10-08 01:53:57 +0100

    Bump CoreJvm Compiler -> `2.0.0-SNAPSHOT.014`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/CoreJvmCompiler.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/CoreJvmCompiler.kt
@@ -46,12 +46,12 @@ object CoreJvmCompiler {
     /**
      * The version used to in the build classpath.
      */
-    const val dogfoodingVersion = "2.0.0-SNAPSHOT.012"
+    const val dogfoodingVersion = "2.0.0-SNAPSHOT.014"
 
     /**
      * The version to be used for integration tests.
      */
-    const val version = "2.0.0-SNAPSHOT.012"
+    const val version = "2.0.0-SNAPSHOT.014"
 
     /**
      * The ID of the Gradle plugin.
@@ -61,7 +61,7 @@ object CoreJvmCompiler {
     /**
      * The library with the [dogfoodingVersion].
      */
-    val pluginLib = pluginLib(dogfoodingVersion)
+    val pluginLib = pluginLibNew(dogfoodingVersion)
 
     /**
      * The library with the given [version].
