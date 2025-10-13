bed0c3333661c67cf20b4f64d889c414f5ff5dba
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-10-13 18:16:27 +0100

    Remove `import org.gradle.kotlin.dsl.resolutionStrategy` as unknown
    Also:
     * Optimise other imports.

--- annotation/src/test/resources/annotator-plugin-test/build.gradle.kts
+++ annotation/src/test/resources/annotator-plugin-test/build.gradle.kts
@@ -25,13 +25,10 @@
  */
 
 import io.spine.dependency.lib.Grpc
-import io.spine.dependency.local.Spine
 import io.spine.dependency.local.Base
 import io.spine.gradle.repo.standardToSpineSdk
 import org.gradle.api.tasks.JavaExec
-import org.gradle.kotlin.dsl.all
 import org.gradle.kotlin.dsl.invoke
-import org.gradle.kotlin.dsl.resolutionStrategy
 
 buildscript {
 
