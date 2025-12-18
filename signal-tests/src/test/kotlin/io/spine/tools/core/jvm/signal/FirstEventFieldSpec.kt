99a69ca820a561a3a7573dedf7975b0a66813429
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-12-18 16:47:23 +0000

    Test that first event and rejection fields are not assumed required

---
+++ signal-tests/src/test/kotlin/io/spine/tools/core/jvm/signal/FirstEventFieldSpec.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.core.jvm.signal
+
+import io.kotest.matchers.string.shouldNotContain
+import java.nio.file.Path
+import kotlin.io.path.Path
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+
+@DisplayName("The first field of an event message should")
+internal class FirstEventFieldSpec {
+
+    companion object : SignalPluginTestSetup() {
+
+        lateinit var eventCode: String
+
+        @BeforeAll
+        @JvmStatic
+        fun setup(@TempDir projectDir: Path) {
+            runPipeline(projectDir)
+            val sourceFile = file(Path("$JAVA_SRC_DIR/event/ObservationMade.java"))
+            eventCode = sourceFile.code()
+        }
+    }
+
+    @Test
+    fun `not be required`() {
+        eventCode shouldNotContain TARGET_ENTITY_ID_MUST_BE_SET
+    }
+}
