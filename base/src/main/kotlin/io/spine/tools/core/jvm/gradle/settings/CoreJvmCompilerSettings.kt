8661d597c3546df700b297a205cafe3a4550ce94
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-06-10 21:00:26 +0100

    Undo introduction of local `MessageFile`
    The move #92 was a mistake. We duplicated the knowlege about well-known message file names. The Base has to have this knowledge, and we should access it from CoreJvm Compiler.

--- base/src/main/kotlin/io/spine/tools/core/jvm/gradle/settings/CoreJvmCompilerSettings.kt
+++ base/src/main/kotlin/io/spine/tools/core/jvm/gradle/settings/CoreJvmCompilerSettings.kt
@@ -30,9 +30,9 @@ package io.spine.tools.core.jvm.gradle.settings
 
 import com.google.common.collect.ImmutableList
 import io.spine.annotation.Internal
+import io.spine.base.MessageFile
 import io.spine.tools.compiler.ast.FilePattern
 import io.spine.tools.compiler.ast.FilePatternFactory
-import io.spine.tools.core.jvm.MessageFile
 import io.spine.tools.core.jvm.gradle.settings.SignalSettings.Companion.DEFAULT_COMMAND_ACTIONS
 import io.spine.tools.core.jvm.gradle.settings.SignalSettings.Companion.DEFAULT_EVENT_ACTIONS
 import io.spine.tools.core.jvm.gradle.settings.SignalSettings.Companion.DEFAULT_REJECTION_ACTIONS
@@ -61,7 +61,7 @@ public class CoreJvmCompilerSettings @Internal public constructor(private val pr
      */
     public val commands: SignalSettings = SignalSettings(
         project,
-        MessageFile.COMMANDS.suffix,
+        MessageFile.COMMANDS.suffix(),
         DEFAULT_COMMAND_ACTIONS
     )
 
@@ -70,7 +70,7 @@ public class CoreJvmCompilerSettings @Internal public constructor(private val pr
      */
     public val events: SignalSettings = SignalSettings(
         project,
-        MessageFile.EVENTS.suffix,
+        MessageFile.EVENTS.suffix(),
         DEFAULT_EVENT_ACTIONS
     )
 
@@ -79,7 +79,7 @@ public class CoreJvmCompilerSettings @Internal public constructor(private val pr
      */
     public val rejections: SignalSettings = SignalSettings(
         project,
-        MessageFile.REJECTIONS.suffix,
+        MessageFile.REJECTIONS.suffix(),
         DEFAULT_REJECTION_ACTIONS
     )
 
