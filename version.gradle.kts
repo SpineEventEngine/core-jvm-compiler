2fca815b3052f655a3a985a474a348fa12101193
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2026-08-03 18:57:39 +0100

    Bump version -> `2.0.0-SNAPSHOT.081`

--- version.gradle.kts
+++ version.gradle.kts
@@ -29,5 +29,6 @@
  *
  * Do not rename this property, as it is also used in the integration tests via its name.
  */
-val coreJvmCompilerVersion by extra("2.0.0-SNAPSHOT.081")
-val versionToPublish by extra(coreJvmCompilerVersion)
+val coreJvmCompilerVersion = "2.0.0-SNAPSHOT.081"
+extra.set("coreJvmCompilerVersion", coreJvmCompilerVersion)
+extra.set("versionToPublish", coreJvmCompilerVersion)
